92796f01e9c66021d12625b8472b879ea5f146d6
Kadacheahmedrami <a_kadache@estin.dz>
2025-01-25 18:47:11 +0100

    the lax

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -32,7 +32,7 @@ router.post('/login', validateLogin, async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: "none",
+      sameSite: "lax",
       path: '/', 
       maxAge: 7* 24 * 60 * 60 * 1000
     });
