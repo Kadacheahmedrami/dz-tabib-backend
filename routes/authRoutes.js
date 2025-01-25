6099bbb2dadaca0b16d5a8e375dd161c0b570515
Kadacheahmedrami <a_kadache@estin.dz>
2025-01-25 19:34:14 +0100

    same site to none

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -32,7 +32,7 @@ router.post('/login', validateLogin, async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: "lax",
+      sameSite: "none",
       path: '/', 
       maxAge: 7* 24 * 60 * 60 * 1000
     });
