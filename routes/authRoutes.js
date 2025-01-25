220566802dc01c8f21a69787612e7ab46fd8d931
Kadacheahmedrami <a_kadache@estin.dz>
2025-01-25 22:29:37 +0100

    testing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -31,8 +31,8 @@ router.post('/login', validateLogin, async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: "none",
+      secure: true,
+      sameSite: "strict",
       path: '/', 
       maxAge: 7* 24 * 60 * 60 * 1000
     });
