760117f9024c5f410d44e6b9a9addbf6dbf55565
Kadacheahmedrami <a_kadache@estin.dz>
2025-01-26 22:49:00 +0100

    pls

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -32,9 +32,7 @@ router.post('/login', validateLogin, async (req, res) => {
     res.cookie('azouaou', token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'None',
-     
-      maxAge: 7* 24 * 60 * 60 * 1000,
+      maxAge: 3600000
       
     });
 
